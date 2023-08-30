2c691a75efd904a5b84711c1f2ebea4a7302f1c0
b-giblin <bgiblin893@gmail.com>
2023-08-30 10:18:43 -0400

    added random module to randomize mac address

--- mac-spoofer.py
+++ mac-spoofer.py
@@ -1,9 +1,16 @@
 import subprocess
+import random
 
 # bring down mac
 config_down = "ifconfig eth0 down"
-# change mac
-change_mac = "ifconfig eth0 hw ether 00:11:22:33:44:66"
+
+# change mac to a random one
+random_mac = ("52:54:00:%02x:%02x:%02x" % (
+        random.randint(0, 255),
+        random.randint(0, 255),
+        random.randint(0, 255),
+        ))
+change_mac = f"ifconfig eth0 hw ether {random_mac}"
 # bring mac back up
 mac_up = "ifconfig eth0 up"
 
---
+++ random-mac.py
@@ -0,0 +1,7 @@
+import random
+
+print("52:54:00:%02x:%02x:%02x" % (
+        random.randint(0, 255),
+        random.randint(0, 255),
+        random.randint(0, 255),
+        ))
