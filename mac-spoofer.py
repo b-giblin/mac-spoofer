ef35805b0e79859336b1c4b90230964f095a7f5c
b-giblin <bgiblin893@gmail.com>
2023-08-31 10:09:07 -0400

    Added user input functionality as well as error handling

--- mac-spoofer.py
+++ mac-spoofer.py
@@ -1,23 +1,43 @@
-import subprocess
+import optparse
 import random
+import subprocess
 
-# bring down mac
-config_down = "ifconfig eth0 down"
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest="interface", help="interface to change its MAC address.")
+
+# specify the interface you wish to change the mac of
+interface = input("Specify the interface: ")
+
+
+try:
+  if interface == 'eth0' or 'wlan0':
+    user_interface = interface
+except:
+  ("Error. Invalid interface specified.")
+
+
 
 # change mac to a random one
 random_mac = ("52:54:00:%02x:%02x:%02x" % (
-        random.randint(0, 255),
-        random.randint(0, 255),
-        random.randint(0, 255),
-        ))
-change_mac = f"ifconfig eth0 hw ether {random_mac}"
+  random.randint(0, 255),
+  random.randint(0, 255),
+  random.randint(0, 255),
+  ))
 # bring mac back up
 mac_up = "ifconfig eth0 up"
 
 
-subprocess.call(config_down, shell=True)
-subprocess.call(change_mac, shell=True)
-subprocess.call(mac_up, shell=True)
+subprocess.call(f"ifconfig {interface} down", shell=True)
+subprocess.call(f"ifconfig {interface} hw ether {random_mac}", shell=True)
+subprocess.call(f"ifconfig {interface} up", shell=True)
+
+
+
+
+
+
+
 
 
 
